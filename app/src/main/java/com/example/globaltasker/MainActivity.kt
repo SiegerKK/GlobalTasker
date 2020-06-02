16247bb549bc566cf5e719021e37ccbf5be733f5
yyevdokimov <e.evdokimov79@gmail.com>
2020-06-02 15:03:23 +0200

    Updated appbar definition

--- app/src/main/java/com/example/globaltasker/MainActivity.kt
+++ app/src/main/java/com/example/globaltasker/MainActivity.kt
@@ -12,7 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+
+        supportActionBar?.title = "Tasks"
 
         // Plus button
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
